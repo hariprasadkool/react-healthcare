0743dcf0b004eb5ae813fbd4289be5ec993114f4
Ginnapu Hari Prasad <ginnapuharipvst@IN.nam.ad.pwcinternal.com>
2019-01-12 13:06:20 +0530

    Added bootstrap to index.html so that is available across the app

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="lander">
+                    <h1>HealthCare</h1>
+                    <p>A healthcare app</p>
+                </div>
+            </div>
+        );
+    }
+}
